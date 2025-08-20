35858fdf6c40e2bdf63416b78950b8b6f8301f55
Peter <peter.warholm@gmail.com>
2025-08-20 21:51:06 +0200

    Add getRandomColor function to generate random hex color code

--- src/components/RandomColorButton.tsx
+++ src/components/RandomColorButton.tsx
@@ -2,6 +2,9 @@ interface RandomColorButtonProps {
     onColorChange: (color: string) => void;
 }
 
+function getRandomColor(): string {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+}
 
 function RandomColorButton({ onColorChange }: RandomColorButtonProps) {
     // Function to generate a random hex color code
