58bc1510271a06284434dcd939ffcdb4cd47a839
Peter <peter.warholm@gmail.com>
2025-08-21 14:48:36 +0200

    Remove debugging log from ColorDisplay component

--- src/components/ColorDisplay.tsx
+++ src/components/ColorDisplay.tsx
@@ -3,13 +3,12 @@ interface ColorDisplayProps {
 }
 
 function ColorDisplay({ color }: ColorDisplayProps) {
-    console.log(`Color displayed: ${color}`); // For debugging purposes
     return (
         <div>
             <p>Aktuell färgkod:</p>
             <p>{color.toUpperCase()}</p>
         </div>
-    )
+    );
 }
 
 export default ColorDisplay;
