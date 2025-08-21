c643bb8ab238f514a8e1c96257b7c53705b73bea
Peter <peter.warholm@gmail.com>
2025-08-21 22:41:03 +0200

    Move App.css import to the correct styles directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import "./App.css";
 import ColorBackground from "./components/ColorBackground";
 import ColorDisplay from "./components/ColorDisplay";
 import RandomColorButton from "./components/RandomColorButton";
+import "./styles/App.css";
 
 function App() {
     const [backgroundColor, setBackgroundcolor] = useState('#FFFFFF');
