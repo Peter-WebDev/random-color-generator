43f773f93c0672132ef561d2d25dbacbd146bdf8
Peter <peter.warholm@gmail.com>
2025-08-21 16:18:03 +0200

    Adding expect statement for background color

--- src/components/ColorBackground.test.tsx
+++ src/components/ColorBackground.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import { describe, it } from "vitest";
+import { describe, expect, it } from "vitest";
 
 describe("ColorBackground", () => {
     it("should apply background color from props", () => {
@@ -9,5 +9,7 @@ describe("ColorBackground", () => {
         // Find element with class name by using getByTestId
         const backgroundDiv = screen.getByTestId('color-background');
 
+        // Using toHaveStyle matching the background color
+        expect(backgroundDiv).toHaveStyle('background-color: #FFAA00');
     });
 });
