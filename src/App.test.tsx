c73d4f70c5fdc9840eac3ecd3ff9313fccb8da30
Peter <peter.warholm@gmail.com>
2025-08-21 21:18:58 +0200

    Update App test to render correct headline and improve initial color state check

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
-import App from "../src/App";
+import App from "./App";
 
 describe("App", () => {
-  it("should renders headline", () => {
+  it("should render headline", () => {
     render(<App />);
-    const headline = screen.getByText("Vite + React");
+    const headline = screen.getByText("Random Color Generator");
     expect(headline).toBeInTheDocument();
+  });
+
+  it("should show initial white color", () => {
+    render(<App />);
+
+    // Test initial color state in ColorDisplay
+    expect(screen.getByText("#FFFFFF")).toBeInTheDocument();
   });
 });
